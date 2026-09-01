33c9642a7206cf5316254ba88174675aa63391bc
Николь Журбенко <kindarufy@yandex.ru>
2026-09-01 21:32:24 +0300

    fix: restore report UI feature

---
+++ kirta-ui/src/features/reports/index.ts
@@ -0,0 +1,5 @@
+export { ReportHeader } from "./ReportHeader";
+export { ReportTabs } from "./ReportTabs";
+export { ScaFindingsTab } from "./ScaFindingsTab";
+export { LangBar } from "./LangBar";
+export { LibrariesDialog } from "./LibrariesDialog";
